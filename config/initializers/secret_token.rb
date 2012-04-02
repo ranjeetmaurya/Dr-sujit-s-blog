464313b23f503ab76e458cb364047b6c4444d63a
Ranjeet <ranjeet@ranjeet-desktop.(none)>
2012-04-02 20:41:14 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonnelWeb::Application.config.secret_token = 'bc4b785ad026a7e09c7592a54bc76f23ee82a1f2b95e49a9579e107cf6f7523db9a25854c36a128b6cf4e2c758519bc9ba82104dcb17d98e3bd88345039e2ae1'
